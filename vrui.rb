052b17e94e2e59a5274457ddc17ca1630f4fc7b0
captcrowbar <126704319+captcrowbar@users.noreply.github.com>
2023-03-01 13:04:45 -0600

    Update vrui.rb

--- vrui.rb
+++ vrui.rb
@@ -29,7 +29,7 @@ class Vrui < Formula
     args << "PKGCONFIGINSTALLDIR=#{lib}/pkgconfig"
     args << "PLUGININSTALLDIR=#{lib}/#{name}"
     args << "SHAREINSTALLDIR=#{share}/#{name}"
-    args << "PNG_BASEDIR=#{(MacOS.version >= :mountain_lion) ? HOMEBREW_PREFIX : MacOS::X11.prefix}"
+    args << "PNG_BASEDIR=#{(MacOS.version >= :mountain_lion) ? HOMEBREW_PREFIX : MacOS::libx11.prefix}"
     #args << "X11_BASEDIR=#{MacOS::X11.prefix}"
     #args << "GL_BASEDIR=#{MacOS::X11.prefix}"
     #args << "GLU_BASEDIR=#{MacOS::X11.prefix}"
