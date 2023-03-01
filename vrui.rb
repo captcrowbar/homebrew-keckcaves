868a7bb5b73678953df900f0b38934a1a5edc867
captcrowbar <126704319+captcrowbar@users.noreply.github.com>
2023-03-01 12:47:10 -0600

    Update vrui.rb

--- vrui.rb
+++ vrui.rb
@@ -33,7 +33,7 @@ class Vrui < Formula
     args << "JPEG_BASEDIR=#{HOMEBREW_PREFIX}"
     args << "TIFF_BASEDIR=#{HOMEBREW_PREFIX}"
     args << "LIBUSB1_BASEDIR=#{HOMEBREW_PREFIX}"
-    #args << "PNG_BASEDIR=#{(MacOS.version >= :mountain_lion) ? HOMEBREW_PREFIX : MacOS::X11.prefix}"
+    args << "PNG_BASEDIR=#{(MacOS.version >= :mountain_lion) ? HOMEBREW_PREFIX : MacOS::X11.prefix}"
     args << "X11_BASEDIR=#{MacOS::X11.prefix}"
     args << "GL_BASEDIR=#{MacOS::X11.prefix}"
     args << "GLU_BASEDIR=#{MacOS::X11.prefix}"
